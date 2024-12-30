6ddffa348c5157e24f50f4158e55d6c808dc57c1
zepteloid <vadimbakulin@gmail.com>
2024-12-30 03:06:46 +0300

    add ex and ind1 files

---
+++ ex.py
@@ -0,0 +1,15 @@
+import sys
+
+if __name__ == '__main__':
+    # Ввести кортеж одной строкой.
+    A = tuple(map(int, input().split()))
+    # Проверить количество элементов кортежа.
+    if len(A) != 10:
+        print("Неверный размер кортежа", file=sys.stderr)
+        exit(1)
+    # Найти искомую сумму.
+    s = 0
+    for item in A:
+        if abs(item) < 5:
+            s += item
+    print(s)
